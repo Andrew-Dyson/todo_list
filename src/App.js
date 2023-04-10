6d7c85c6bed682df9f54eba59f195b883a8de079
Andrew Dyson <axd659@gmail.com>
2023-04-10 21:05:07 +0100

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [todoitems, setTodoItems] = useState([
+    { name : "Buy Shopping", id:1},
+    { name : "Clean Bathroom", id:2},
+    { name : "Car's MOT", id:3},
+  ]);
+ 
+  const [newToDoItem, setNewToDo] = useState("");
+
+  const handleToDoInput = (event) => {
+    setNewToDo(event.target.value)
+  }
+
+  const handleToDoSubmit = (event) => {
+    event.preventDefault();
+    const copyToDos = [...todoitems]
+    copyToDos.push({
+      name : newToDoItem,
+      id: Date.now()
+    })
+    setTodoItems(copyToDos)
+    setNewToDo("")
+  }
+
+
+return(
+<>
+<h1> To Do List </h1>
+<ul>
+{todoitems.map(todoitem => {
+  return(
+  <li key = {todoitem.id}><span>{todoitem.name}</span></li>
+  )
+})}
+</ul>
+<form onSubmit = {handleToDoSubmit}>        
+        <label htmlFor="new-todo"> Add a new task: </label>   
+        <input type="text" id="new-todo" onChange = {handleToDoInput} value = {newToDoItem}/>    
+        <button> Save To Do </button>             
+            
+</form>
+</>
+)
+};
 
 export default App;
